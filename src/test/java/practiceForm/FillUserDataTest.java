3a717a52bb8debb948b5a51b91c9e851be3c6adb
Nadya Frolova <n.frolova.kzn@yandex.ru>
2023-01-10 17:50:14 +0300

    1) поправлен шаг с чекбоксом Female; 2) добавлены проверки соответствия введенных данных тем данным, которые отображены в заполненной форме студента.

--- src/test/java/practiceForm/FillUserDataTest.java
+++ src/test/java/practiceForm/FillUserDataTest.java
@@ -1,15 +1,18 @@
+package practiceForm;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeForm {
+public class FillUserDataTest {
     File file = new File("src/test/resources/file.jpg");
     @Test
-    void practiceFormTest(){
+    void successfulFillUserDataTest(){
         open("https://demoqa.com/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
@@ -17,7 +20,7 @@ public class PracticeForm {
         $("#firstName").setValue("Надежда");
         $("#lastName").setValue("Фролова");
         $("#userEmail").setValue("test@yandex.ru");
-        $("#genterWrapper").shouldHave(text("Female")).click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("9127420338");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-dropdown-container").$(byText("November")).click();
@@ -33,6 +36,16 @@ public class PracticeForm {
         $("#city").$(byText("Delhi")).click();
         $("#submit").click();
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(exactText("Надежда Фролова"));
+        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(exactText("test@yandex.ru"));
+        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(exactText("Female"));
+        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(exactText("9127420338"));
+        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(exactText("04 November,1984"));
+        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(exactText("Maths"));
+        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(exactText("Sports"));
+        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(exactText("file.jpg"));
+        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(exactText("Актобе"));
+        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(exactText("NCR Delhi"));
 
-            }
+    }
 }
